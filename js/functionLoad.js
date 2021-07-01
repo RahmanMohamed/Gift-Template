d57be6233b3bf466e7386e73fb079d45a5d0117b
RahmanMohamed <xrahman10@gmail.com>
2021-07-01 17:45:29 +0200

    Added load screen as index

--- js/functionLoad.js
+++ js/functionLoad.js
@@ -2,8 +2,8 @@ let homeBtn = document.getElementById('homeButton');
 let skipButton = document.getElementById('skipButton');
 
 homeBtn.addEventListener('click',function(){
-    window.location.assign('index.html')
+    window.location.assign('indexHome.html')
 });
 skipButton.addEventListener('click',function(){
-    window.location.assign('index.html')
+    window.location.assign('indexHome.html')
 });
