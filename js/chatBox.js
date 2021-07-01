e600e40acdd4c6f9bef1c6301baa5ee97df52525
RahmanMohamed <xrahman10@gmail.com>
2021-07-01 16:29:11 +0200

    Update1

--- js/chatBox.js
+++ js/chatBox.js
@@ -1,23 +1,28 @@
 /**********chat box**********/ 
 // opening chat
-function openMsgBox()
-{
-    document.querySelector('.msg-btn').style.opacity ='0' ;
-    document.querySelector('.chat-box').style.clipPath = 'circle(75%'
-    document.querySelector('.msg-btn').style.transitionDelay ='0s' ;
-}
-// closing chat box 
-function closeChat()
-{
-    document.querySelector('.chat-box').style.clipPath = 'circle(0% at 90% 95%' 
-    document.querySelector('.msg-btn').style.opacity ='1' ;
-    document.querySelector('.msg-btn').style.transitionDelay ='0.5s' ;
+// function openMsgBox()
+// {
+//     document.querySelector('.msg-btn').style.opacity ='0' ;
+//     // document.querySelector('.chat-box').style.clipPath = 'circle(75%'
+//     document.querySelector('.msg-btn').style.transitionDelay ='0s' ;
+// }
+// // closing chat box 
+// function closeChat()
+// {
+//     // document.querySelector('.chat-box').style.clipPath = 'circle(0% at 90% 95%' 
+//     document.querySelector('.msg-btn').style.opacity ='1' ;
+//     document.querySelector('.msg-btn').style.transitionDelay ='0.5s' ;
 
-}
-// send msg
-function sendMsg()
-{
-    document.querySelector('.chat-box').style.clipPath = 'circle(75%'
-    document.getElementById("msg").value = "" ;
+// }
+// // send msg
+// function sendMsg()
+// {
+//     document.querySelector('.chat-box').style.clipPath = 'circle(75%'
+//     document.getElementById("msg").value = "" ;
      
-}  
+// }  
+let chatBox=document.getElementById('chat-box');
+let chatBtn=document.getElementById('msg-btn');
+chatBtn.addEventListener('click',function(){
+    $('#chat-box').toggle(1000)
+})
